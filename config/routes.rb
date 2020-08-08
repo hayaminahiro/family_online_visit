abf3d0b3a2a7e916b6f5c4ab5291684555c6d7c1
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-08 12:00:01 +0900

    informationのMVC周りを作成した

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :reservations
   end
-  
+
   resources :residents
+  resources :informations
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_08_020040) do
+ActiveRecord::Schema.define(version: 2020_08_08_024857) do
+
+  create_table "information", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "news"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "relatives", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
