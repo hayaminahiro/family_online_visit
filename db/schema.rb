76a9e7ecc8578698b74f1df7b34dde9cd3b32729
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-08 10:46:13 +0900

    residentのMVC周りを作成した

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_07_040738) do
+ActiveRecord::Schema.define(version: 2020_08_07_061153) do
+
+  create_table "residents", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "floor", null: false
+    t.string "charge_worker"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
