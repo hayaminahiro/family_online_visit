75d9d3dd44b6ab29ce516c8e9bc83dea5f3aaec5
Hayato Saito <omimisama@gmail.com>
2020-08-14 10:23:22 +0900

    ヘッダーリンク表示

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
+  root 'static_pages#top'
+
   resources :users do
     resources :reservations
   end
 
   resources :residents
   resources :informations
+
 end
