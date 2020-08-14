608f0f6222b5ce909ec461c4a06001baef2a6484
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-08-14 17:24:26 +0900

    routesのusersにmember do get :video_roomを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :users do
+    member do
+      get :video_room
+    end
     resources :reservations
   end
+
+
 
   resources :residents
   resources :informations
