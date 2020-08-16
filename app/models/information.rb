a268fb4d33e2b5a669f93a1f8d4b088dafb7daba
永野高博 <naganotakahiro@mbp.local>
2020-08-16 22:09:56 +0900

    newsカラムにuniquenessのバリデーションを追加した

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,5 +1,5 @@
 class Information < ApplicationRecord
-  validates :news, presence: true
+  validates :news, presence: true, uniqueness: true
   validates :title, presence: true
 
   enum status: { others: 0, head: 1 }
