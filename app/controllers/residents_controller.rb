fe0e7fdb12c67d5f8c22e643f694913a0df12a02
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-20 13:00:01 +0900

    入居者の作成・編集・削除機能を追加

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -1,10 +1,39 @@
 class ResidentsController < ApplicationController
-  def new
-  end
-
-  def edit
-  end
-
   def index
+    @residents = Resident.all
+    @resident = Resident.new
+  end
+
+  def create
+    @resident = Resident.new(resident_params)
+    if  @resident.save
+      flash[:success] = "入居者を新規登録できました"
+    else
+      flash[:danger] = "入居者登録できませんでした。入力内容をご確認ください"
+    end
+    redirect_to residents_path
+  end
+
+  def update
+    @resident = Resident.find(params[:id])
+    if @resident.update(resident_params)
+      flash[:success] = "入居者情報を更新できました"
+    else
+      flash[:danger] = "更新できませんでした。入力内容をご確認ください"
+    end
+    redirect_to residents_path
+  end
+
+  def destroy
+    @resident = Resident.find(params[:id])
+    @resident.destroy
+    flash[:danger] = "入居者情報を削除しました"
+    redirect_to residents_path
+  end
+
+  private
+
+  def resident_params
+    params.require(:resident).permit(:name, :floor, :charge_worker)
   end
 end
