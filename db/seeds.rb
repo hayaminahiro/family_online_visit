04a6456454e626999a1e6c4755230795bc46ea17
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-23 07:29:28 +0900

    seeds.rbでデフォルトの施設を3つ作成できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
 Information.create!( title: "当システムの利用方法",
                       news: "ご利用前にご入居者様の・・・",
                     status: 1)
+
+Facility.create!( name: "施設A")
+Facility.create!( name: "施設B")
+Facility.create!( name: "施設C")
