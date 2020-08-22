804f3bc0ffa86ce5bd8349b3dc338cf556900c24
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-23 07:23:48 +0900

    施設テーブル追加により、入居者ページ・コントローラの修正をした

--- app/controllers/residents_controller.rb
+++ app/controllers/residents_controller.rb
@@ -6,29 +6,31 @@ class ResidentsController < ApplicationController
 
   def create
     @resident = Resident.new(resident_params)
+    @resident.facility_id = params[:facility_id].to_i  #追加
     if  @resident.save
       flash[:success] = "入居者を新規登録できました"
     else
       flash[:danger] = "入居者登録できませんでした。入力内容をご確認ください"
     end
-    redirect_to residents_path
+    redirect_to facility_residents_path
   end
 
   def update
     @resident = Resident.find(params[:id])
+    @resident.facility_id = params[:facility_id].to_i #追加
     if @resident.update(resident_params)
       flash[:success] = "入居者情報を更新できました"
     else
       flash[:danger] = "更新できませんでした。入力内容をご確認ください"
     end
-    redirect_to residents_path
+    redirect_to facility_residents_path
   end
 
   def destroy
     @resident = Resident.find(params[:id])
     @resident.destroy
     flash[:danger] = "入居者情報を削除しました"
-    redirect_to residents_path
+    redirect_to facility_residents_path
   end
 
   private
--- db/migrate/20200807061153_create_residents.rb
+++ db/migrate/20200807061153_create_residents.rb
@@ -1,6 +1,7 @@
 class CreateResidents < ActiveRecord::Migration[5.2]
   def change
     create_table :residents do |t|
+      t.integer :facility_id
       t.string :name, null: false
       t.integer :floor, null: false
       t.string :charge_worker
