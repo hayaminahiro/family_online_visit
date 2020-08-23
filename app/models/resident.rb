71668de6833b48b7a14071abc064c4196202be19
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-08-23 21:10:25 +0900

    バリデーションを追加

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -1,4 +1,8 @@
 class Resident < ApplicationRecord
   has_many :relatives, dependent: :destroy
   has_many :users, through: :relatives
+
+  validates :name, presence: true
+  validates :floor, presence: true
+  validates :charge_worker, presence: true
 end
