be9d2f85111563bfba117ca45522196f62c73e5e
Hayato Saito <omimisama@gmail.com>
2020-08-25 05:45:56 +0900

    remember_meをonに

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -3,4 +3,9 @@ class Facility < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # cookieでログイン情報を保持
+  def remember_me
+    true
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # cookieでログイン情報を保持
+  def remember_me
+    true
+  end
+
   has_many :relatives, dependent: :destroy
   has_many :residents, through: :relatives
   has_many :reservations, dependent: :destroy
