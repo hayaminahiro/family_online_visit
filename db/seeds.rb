2ecee3f6786b1e4505122cc92f676ba1849d6bb3
Hayato Saito <omimisama@gmail.com>
2020-08-27 09:25:39 +0900

    seed情報追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,24 @@ Information.create!(title: "当システムの利用方法",
                       news: "ご利用前にご入居者様の・・・",
                     status: 1)
 
-Facility.create!(facility_name: "老人ホーム-A",
+# deviseの場合、~.save!しないとpasswordがseedに反映されない。
+home = Facility.create!(facility_name: "老人ホーム-A",
              email: "home@email.com",
              password: "password"
 )
+home.save!
 
-Facility.create!(facility_name: "病院-B",
+hospital =  Facility.create!(facility_name: "病院-B",
                  email: "hospital@email.com",
                  password: "password"
 )
+hospital.save!
 
-Facility.create!(facility_name: "保育園-C",
+hoiku = Facility.create!(facility_name: "保育園-C",
                  email: "hoiku@email.com",
                  password: "password"
 )
+hoiku.save!
 
 99.times do |n|
   name = "sampleご家族-#{n+1}"
@@ -28,5 +32,6 @@ end
 
 99.times do |n|
   name = "入居者-#{n+1}"
-  Resident.create!(name: name)
+  Resident.create!(name: name,
+                   charge_worker: "担当者-#{n+1}")
 end
