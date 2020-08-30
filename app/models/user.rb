9dde033d4e5ae4d06186f6459e6ef1aed21f6986
yuyatoyotomi <toyo.yuya0311@gmail.com>
2020-08-30 10:53:20 +0900

    googleログイン完了、lineログイン試行錯誤前

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,24 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i[google line]
   has_many :relatives, dependent: :destroy
   has_many :residents, through: :relatives
   has_many :reservations, dependent: :destroy
+
+  protected
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(name:     auth.info.name,
+                         email: auth.info.email,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20],
+                         meta:     auth.to_yaml)
+    end
+    user
+  end
 end
