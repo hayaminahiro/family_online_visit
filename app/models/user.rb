adc3b26eb8ccfc381f1a878562d9993c692ac1bb
yuyatoyotomi <toyo.yuya0311@gmail.com>
2020-09-02 10:01:30 +0900

    jj

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i[google line]
+         :recoverable, :rememberable, :validatable, :trackable, :omniauthable, omniauth_providers: %i[google_oauth2 line]
   has_many :relatives, dependent: :destroy
   has_many :residents, through: :relatives
   has_many :reservations, dependent: :destroy
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -11,8 +11,8 @@
 Devise.setup do |config|
   require 'devise/orm/active_record'
   config.omniauth :google_oauth2,
-                  ENV['GOOGLE_APP_ID'], 
-                  ENV['GOOGLE_APP_SECRET'], 
+                  ENV['GOOGLE_APP_ID'],
+                  ENV['GOOGLE_APP_SECRET'],
                   name: :google
 
   config.omniauth :line,
