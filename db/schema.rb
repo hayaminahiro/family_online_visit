802a897240699e5d4a3f9f4aafdb49278987f6b2
Hayato Saito <omimisama@gmail.com>
2020-09-10 01:09:06 +0900

    facility_adminカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_06_152730) do
+ActiveRecord::Schema.define(version: 2020_09_09_160630) do
 
   create_table "facilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_06_152730) do
     t.datetime "updated_at", null: false
     t.string "facility_name"
     t.boolean "admin", default: false
+    t.boolean "facility_admin", default: false
     t.index ["email"], name: "index_facilities_on_email", unique: true
     t.index ["reset_password_token"], name: "index_facilities_on_reset_password_token", unique: true
   end
