f698924a155e8d9829398964374c2923a0436711
yuyatoyotomi <toyo.yuya0311@gmail.com>
2020-09-10 17:51:28 +0900

    google,lineログイン同意画面はクリア。ログインは不可

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -15,7 +15,8 @@ Devise.setup do |config|
   config.omniauth :line,
                   ENV['LINE_KEY'],
                   ENV['LINE_SECRET'],
-                  scope: 'openid'
+                  scope: 'openid',
+                  redirect_uri: "https://4398e57bb92c.ngrok.io/users/auth/line/callback"
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
