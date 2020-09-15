96a27e389efa2b64a241f5e6db49c50cfdf48006
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-09-15 19:41:33 +0900

    パスワード入力なしで編集できるように変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,20 @@ class User < ApplicationRecord
     true
   end
 
+  # パスワード入力なしでご家族情報編集可能にするため追加
+  def update_without_current_password(params, *options)
+    params.delete(:current_password)
+
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update_attributes(params, *options)
+    clean_up_passwords
+    result
+  end
+
   protected
   def self.from_omniauth(auth)
     user = User.find_by(email: auth.info.email)
