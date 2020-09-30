5185ee04ab3aaa158d3eac23abb2be895fa3b2d5
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-09-30 23:07:56 +0900

    RequestResidentモデルにバリデーションとenumを追加した

--- app/models/request_resident.rb
+++ app/models/request_resident.rb
@@ -1,4 +1,10 @@
 class RequestResident < ApplicationRecord
   belongs_to :user
   belongs_to :facility
+
+  validates :req_name, presence: true
+  validates :req_phone, presence: true
+  validates :req_address, presence: true
+
+  enum req_approval: { "申請中": 0, "登録済": 1, "拒否": 2 }
 end
