04f9c38ad1da13bb431beb32632010be0bfac98f
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-13 14:44:08 +0900

    aliasとモーダルフォームを表示する際に使用されるメソッドを追加した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,23 @@
 module ApplicationHelper
-    def resource_name
-        :user
-     end
-     
-     def resource
-        @resource ||= User.new
-     end
-     
-     def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-     end
+   def resource_name
+      :user
+   end
+
+   def resource
+      @resource ||= User.new
+   end
+
+   def devise_mapping
+      @devise_mapping ||= Devise.mappings[:user]
+   end
+
+   def modal_title(content)
+      content.id.present? ? "入居者情報編集" : "入居者新規登録"
+   end
+
+   def component(component_name, locals = {}, &block)
+      render("residents/#{component_name}", locals, &block)
+   end
+
+   alias :c :component
 end
