dfd81e9af210a1281155190925df9150b8ea1a03
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-10-16 01:49:46 +0900

    登録申請一覧表示途中

--- app/controllers/facilities_controller.rb
+++ app/controllers/facilities_controller.rb
@@ -41,6 +41,7 @@ class FacilitiesController < ApplicationController
       @users = @users.where('name LIKE ?', "%#{params[:search]}%").paginate(page: params[:page], per_page: 30).order(:id)
     end
     @info_top = Information.find_by(status: "head")
+    @request_residents = RequestResident.where(req_approval: "申請中").where(facility_id: current_facility)
   end
 
   def change_admin
