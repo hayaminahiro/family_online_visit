16adc36970c1e1ae4bdbfc84e3d8deb64faf0029
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-19 18:37:18 +0900

    お知らせ表示する際のグループ分けのメソッドを追加した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,8 @@ module ApplicationHelper
      def devise_mapping
         @devise_mapping ||= Devise.mappings[:user]
      end
+
+     def grouped_facility(information)
+      facilities_grouped = information.group_by(&:facility_id)
+     end
 end
