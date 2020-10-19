653fc02e5a882d76122b359d6e0d33269071995c
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-20 06:42:45 +0900

    インデント の修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,17 +1,17 @@
 module ApplicationHelper
-    def resource_name
-        :user
-     end
-     
-     def resource
-        @resource ||= User.new
-     end
-     
-     def devise_mapping
-        @devise_mapping ||= Devise.mappings[:user]
-     end
+  def resource_name
+    :user
+  end
 
-     def grouped_facility(information)
-      facilities_grouped = information.group_by(&:facility_id)
-     end
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def grouped_facility(information)
+    information.group_by(&:facility_id)
+  end
 end
