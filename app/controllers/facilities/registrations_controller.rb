f2129426f5e068f042b258ec57bfeaedd4d540ad
Hayato Saito <omimisama@gmail.com>
2020-10-22 16:07:57 +0900

    アカウント編集ページ整理完了

--- app/controllers/facilities/registrations_controller.rb
+++ app/controllers/facilities/registrations_controller.rb
@@ -42,12 +42,13 @@ class Facilities::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :facility_name, :image, :icon, :remove_image, :remove_icon])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute, :facility_name])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:attribute, :facility_name, :image, :icon, :remove_image, :remove_icon])
+    devise_parameter_sanitizer.permit(:account_update, keys:
+        [:attribute, :facility_name, :image, :icon, :remove_image, :remove_icon, :image_cache, :icon_cache])
   end
 
   # The path used after sign up.
