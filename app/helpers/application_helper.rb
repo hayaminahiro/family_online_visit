40aa328dbb19fd36df3b373c28caebc0b5039237
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-10-27 20:47:42 +0900

    resident_helperに定義を移動した

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,17 +11,6 @@ module ApplicationHelper
     @devise_mapping ||= Devise.mappings[:user]
   end
 
-  def modal_title(content)
-    content.id.present? ? "入居者情報編集" : "入居者新規登録"
-  end
-
-
-  def component(component_name, locals = {}, &block)
-    render("residents/#{component_name}", locals, &block)
-  end
-
-  alias :c :component
-
   def grouped_facility(information)
     information.group_by(&:facility_id)
   end
