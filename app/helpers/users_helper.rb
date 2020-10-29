c357f922582b678443e485be62b90b192dd13deb
yuya0311 <toyo.yuya0311@gmail.com>
2020-10-29 21:03:56 +0900

    ログイン画面のエラー問題解決、user_editのプロフ登録プレビュー完成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,18 @@
 module UsersHelper
+  # マイページ表示用
   def user_icon_image(user)
     if user.image?
-      image_tag user.image.url, id: :icon_prev, class: "user-profile-image"
+      image_tag user.image.url, id: :img_prev, class: "user-profile-image"
     else
-      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :icon_prev, class: "user-profile-image"
+      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-profile-image"
+    end
+  end
+  # ユーザー編集画面用
+  def user_icon_edit(user)
+    if user.image?
+      image_tag user.image.url, id: :img_prev, class: "user-edit-image image is-rounded is-128x128", style: "border-radius:100%;"
+    else
+      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-profile-image image is-rounded is-128x128", style: "border-radius:100%;"
     end
   end
 end
