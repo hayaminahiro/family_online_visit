9d63363752eaef912d1d111f086d259071092e1c
Hayato Saito <omimisama@gmail.com>
2020-11-02 14:21:44 +0900

    複数画像プレビューのキャッシュ(登録はされない)

--- app/models/memory.rb
+++ app/models/memory.rb
@@ -3,7 +3,7 @@ class Memory < ApplicationRecord
   has_many :images, dependent: :destroy
   accepts_nested_attributes_for :images, allow_destroy: true
 
-  validates :images,  presence: true
+  # validates :images,  presence: true
   validates :title,   presence: true, length: {maximum: 20}
   validates :message, presence: true, length: {minimum: 10}
 
