db5b806ae88d51b8d07586b196ee41fb4f61b9cf
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-05 20:07:58 +0900

    memories_controllerの修正

--- app/controllers/memories_controller.rb
+++ app/controllers/memories_controller.rb
@@ -1,48 +1,42 @@
 class MemoriesController < ApplicationController
+  before_action :set_resident, only: %i[index new create]
 
   def index
-    @resident = current_facility.residents.find(params[:id])
-    # @residents = Resident.all
-    @memories = @resident.memories
+    @memories = current_facility.residents.includes(:memories).where(id: params[:resident_id])
   end
 
   def show
-    @resident = current_facility.residents.find(params[:resident_id])
-    @memories = @resident.memories
-    @memory = @memories.find(params[:id])
+    @memory = Memory.find(params[:id])
   end
 
   def new
-    @resident = current_facility.residents.find(params[:resident_id])
     @memory = @resident.memories.new
+    @memories = current_facility.residents.includes(:memories)
   end
 
   def create
-    @resident = current_facility.residents.find(params[:resident_id])
     @memory = @resident.memories.new(memories_params)
     if @memory.save
-      flash[:notice] = "#{@resident.name}さんの思い出アルバムを投稿しました。"
-      # redirect_to facility_resident_path(id: @resident.id)
-      redirect_to facility_resident_memories_path
+      redirect_to memories_url(resident_id: @resident), notice: "#{@resident.name}さんの思い出アルバムを投稿しました。"
     else
       render :new
     end
   end
 
   def destroy
-    resident = current_facility.residents.find(params[:resident_id])
-    memories = resident.memories
-    @memory = memories.find(params[:id])
+    @memory = Memory.find(params[:id])
     @memory.delete
-    @memory.remove_images!
-    flash[:alert] = "思い出アルバムを削除しました"
-    redirect_to facility_resident_memories_path
+    @memory.remove_r_images!
+    redirect_to memories_url(resident_id: params[:resident_id]), alert: "思い出アルバムを削除しました"
   end
 
     private
 
       def memories_params
-        params.require(:memory).permit(:title, :message, :event_date, {images: []}, {remove_images: []}, :images_cache)
+        params.require(:memory).permit(:title, :message, :event_date, {r_images: []}, {remove_images: []}, :images_cache)
       end
 
+      def set_resident
+        @resident = Resident.find(params[:resident_id])
+      end
 end
