0ed21caa50af638914d246a3f43674ff852ee0f8
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-05 20:03:33 +0900

    カラム追加位置の修正のため、マイグレーションファイルに追記

--- db/migrate/20200905130453_add_facility_id_to_residents.rb
+++ db/migrate/20200905130453_add_facility_id_to_residents.rb
@@ -1,5 +1,5 @@
 class AddFacilityIdToResidents < ActiveRecord::Migration[5.2]
   def change
-    add_reference :residents, :facility, foreign_key: true
+    add_reference :residents, :facility, foreign_key: true, after: :id
   end
 end
--- db/migrate/20200925190818_create_request_residents.rb
+++ db/migrate/20200925190818_create_request_residents.rb
@@ -1,12 +1,12 @@
 class CreateRequestResidents < ActiveRecord::Migration[5.2]
   def change
     create_table :request_residents do |t|
+      t.references :user, foreign_key: true
+      t.references :facility, foreign_key: true
       t.string :req_name
       t.string :req_phone
       t.string :req_address
       t.integer :req_approval, default: 0
-      t.references :user, foreign_key: true
-      t.references :facility, foreign_key: true
 
       t.timestamps
     end
