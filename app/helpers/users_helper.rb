93df7f2b69f4152959b37d0ea18e423f8dd9512f
yuya0311 <toyo.yuya0311@gmail.com>
2020-11-06 18:15:55 +0900

    ドラッグ＆ドロップ機能完成,cache完成

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,14 +10,21 @@ module UsersHelper
   # ユーザー編集画面用
   def user_icon_edit(user)
     if user.image?
-      image_tag user.image.url, id: :img_prev, class: "user-edit-image", style: "width: 210px;
+      image_tag user.image.url, id: :img_prev, class: "user-edit-image", style: "
+      width: 210px;
       height: 210px;
       border-radius: 50%;
       display: inline-block;
       object-fit: cover;
       margin-top:20px;"
     else
-      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-profile-image image is-rounded is-128x128", style: "border-radius:100%;"
+      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-edit-image", style: "
+      width: 210px;
+      height: 210px;
+      border-radius: 50%;
+      display: inline-block;
+      object-fit: cover;
+      margin-top:20px;"
     end
   end
 end
