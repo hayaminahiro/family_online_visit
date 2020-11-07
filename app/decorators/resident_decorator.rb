dce8aadc7ad71d357679958672c1dea73f3469ce
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-07 23:51:45 +0900

    ResidentDecoratorの修正

--- app/decorators/resident_decorator.rb
+++ app/decorators/resident_decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ResidentDecorator
-  def modal_title(content)
-    content.id.present? ? "入居者情報編集" : "入居者新規登録"
+  def modal_title
+    id.present? ? "入居者情報編集" : "入居者新規登録"
   end
 end
