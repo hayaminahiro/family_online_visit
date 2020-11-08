52656d1e20ff00d1b9f8146ee785ae9c461491ac
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-09 07:38:04 +0900

    InformationDecoratorの修正しキャッシュが表示されるようにした

--- app/decorators/information_decorator.rb
+++ app/decorators/information_decorator.rb
@@ -2,10 +2,10 @@
 
 module InformationDecorator
   def information_image
-    return image_tag "https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/info.jpg", id: :img_prev, :size => '500x500' if id == nil
-
-    info = Information.find(id)
-    return image_tag info.image.url, id: :img_prev, :size => '500x500' if info.image?
-    image_tag "https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/info.jpg", id: :img_prev, :size => '500x500'
+    if image?
+      image_tag image.url, id: :img_prev, :size => '500x500'
+    else
+      image_tag "https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/info.jpg", id: :img_prev, :size => '500x500'
+    end
   end
 end
