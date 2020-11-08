2bfd9947f975d8b9cd000a34e4cae7aacd70d887
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-09 07:47:18 +0900

    FacilityDecoratorの修正をしてキャッシュができるようにした

--- app/decorators/facility_decorator.rb
+++ app/decorators/facility_decorator.rb
@@ -2,9 +2,8 @@
 
 module FacilityDecorator
   def facility_season_image
-    facility = Facility.find(id)
-    if facility.image?
-      image_tag facility.image.url, id: :img_prev, :size => '500x500'
+    if image?
+      image_tag image.url, id: :img_prev, :size => '500x500'
     else
       month = Date.today.month
       if month.in?([ 12, 1, 2 ])
@@ -20,9 +19,8 @@ module FacilityDecorator
   end
 
   def facility_icon_image
-    facility = Facility.find(id)
-    if facility.icon?
-      image_tag facility.icon.url, id: :icon_prev, class: "icon_circle"
+    if icon?
+      image_tag icon.url, id: :icon_prev, class: "icon_circle"
     else
       image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/facility_default.png', id: :icon_prev, class: "icon_circle"
     end
