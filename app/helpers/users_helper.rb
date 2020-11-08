f9229c6ccd52ff5d69cdc524f4e82bb11a776c87
yuya0311 <toyo.yuya0311@gmail.com>
2020-11-08 21:25:08 +0900

    styleからcssへ移行完了

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -10,21 +10,9 @@ module UsersHelper
   # ユーザー編集画面用
   def user_icon_edit(user)
     if user.image?
-      image_tag user.image.url, id: :img_prev, class: "user-edit-image", style: "
-      width: 210px;
-      height: 210px;
-      border-radius: 50%;
-      display: inline-block;
-      object-fit: cover;
-      margin-top:20px;"
+      image_tag user.image.url, id: :img_prev, class: "user-edit-image"
     else
-      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-edit-image", style: "
-      width: 210px;
-      height: 210px;
-      border-radius: 50%;
-      display: inline-block;
-      object-fit: cover;
-      margin-top:20px;"
+      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-edit-image"
     end
   end
 end
