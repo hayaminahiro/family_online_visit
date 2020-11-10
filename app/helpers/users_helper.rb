5471c4c6881207416aeae8dddcd0bd3b1ce8f0d3
yuya0311 <toyo.yuya0311@gmail.com>
2020-11-10 12:53:59 +0900

    helperからdecoratorへ移行

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,2 @@
 module UsersHelper
-  # マイページ表示用
-  def user_icon_image(user)
-    if user.image?
-      image_tag user.image.url, id: :img_prev, class: "user-profile-image"
-    else
-      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-profile-image"
-    end
-  end
-  # ユーザー編集画面用
-  def user_icon_edit(user)
-    if user.image?
-      image_tag user.image.url, id: :img_prev, class: "user-edit-image"
-    else
-      image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/user_default.png', id: :img_prev, class: "user-edit-image"
-    end
-  end
 end
