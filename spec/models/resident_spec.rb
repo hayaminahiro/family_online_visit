4c86acda30518ba42e6274d053432865e1a07b92
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-11 14:48:54 +0900

    residentモデルのテストを追加

--- spec/models/resident_spec.rb
+++ spec/models/resident_spec.rb
@@ -1,5 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Resident, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:resident) { create(:resident) }
+
+  describe 'validations' do
+    # 名前、担当者名があれば有効な状態であること
+    it "is valid with a name, email, and password" do
+      expect(resident).to be_valid
+    end
+
+    # 名前がなければ無効な状態であること
+    it "is invalid without a name" do
+      resident.name = ''
+      resident.valid?
+      expect(resident.errors[:name]).to include("を入力してください")
+    end
+
+    # 名前が20文字以上であれば登録できないこと
+    it "is invalid if the name is longer than 50 characters" do
+      resident.name = "あ" * 21
+      resident.valid?
+      expect(resident.errors[:name]).to include("は20文字以内で入力してください")
+    end
+
+    # 担当者名がなければ無効な状態であること
+    it "is invalid without a charge_worker" do
+      resident.charge_worker = ''
+      resident.valid?
+      expect(resident.errors[:charge_worker]).to include("を入力してください")
+    end
+
+    # 担当者名が20文字以上であれば登録できないこと
+    it "is invalid if the charge_worker is longer than 50 characters" do
+      resident.charge_worker = "あ" * 21
+      resident.valid?
+      expect(resident.errors[:charge_worker]).to include("は20文字以内で入力してください")
+    end
+  end
 end
