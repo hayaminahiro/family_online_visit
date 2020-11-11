50c775ae81d74cda4bade9f9e32663a98bbd6e3b
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-11 12:04:21 +0900

    userのモデルスペックテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :user do
-    
+    sequence(:name)     { |n| "sample#{n}" }
+    sequence(:email)    { |n| "TEST#{n}@example.com" }
+    phone               { "08012345678" }
+    password            { "password" }
+    postal_code         { "1234567" }
+    prefecture_name     { "Tokyo" }
+    address_city        { "tyuuou" }
+    address_street      { "nihonbashi" }
+    room_name           { "online_room" }
   end
 end
