40d724c5acb005aef0cc06f1c05f3804237f2fd4
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-11 14:32:30 +0900

    user_specの並び順を修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,60 +16,18 @@ RSpec.describe User, type: :model do
       expect(user.errors[:name]).to include("を入力してください")
     end
 
-    # メールアドレスがなければ無効な状態であること
-    it "is invalid without a email" do
-      user.email = ''
-      user.valid?
-      expect(user.errors[:email]).to include("が入力されていません。")
-    end
-
-    # パスワードがなければ無効な状態であること
-    it "is invalid without a password" do
-      user.password = ''
-      user.valid?
-      expect(user.errors[:password]).to include("が入力されていません。")
-    end
-
-    # メールアドレスがなければ無効な状態であること
-    it "is invalid without a email" do
-      user.email = ''
-      user.valid?
-      expect(user.errors[:email]).to include("が入力されていません。")
-    end
-
-    # 郵便番号がなければ無効な状態であること
-    it "is invalid without a postal_code" do
-      user.postal_code = ''
-      user.valid?
-      expect(user.errors[:postal_code]).to include("を入力してください")
-    end
-
-    # 都道府県がなければ無効な状態であること
-    it "is invalid without a prefecture_name" do
-      user.prefecture_name = ''
-      user.valid?
-      expect(user.errors[:prefecture_name]).to include("を入力してください")
-    end
-
-    # 住所がなければ無効な状態であること
-    it "is invalid without a address_city" do
-      user.address_city = ''
-      user.valid?
-      expect(user.errors[:address_city]).to include("を入力してください")
-    end
-
-    # 建物名がなければ無効な状態であること
-    it "is invalid without a address_street" do
-      user.address_street  = ''
-      user.valid?
-      expect(user.errors[:address_street ]).to include("を入力してください")
-    end
-
     # 名前が20文字以上であれば登録できないこと
     it "is invalid if the name is longer than 50 characters" do
       user.name = "あ" * 21
       user.valid?
       expect(user.errors[:name]).to include("は20文字以内で入力してください")
+    end
+
+    # メールアドレスがなければ無効な状態であること
+    it "is invalid without a email" do
+      user.email = ''
+      user.valid?
+      expect(user.errors[:email]).to include("が入力されていません。")
     end
 
     # メールアドレスが101文字以上であれば登録できないこと
@@ -96,27 +54,36 @@ RSpec.describe User, type: :model do
     # 重複したメールアドレスなら無効な状態であること
     it "is invalid if email is duplicated" do
       User.create(
-        name:            "Sumner",
-        email:           "tester@example.com",
-        phone:           "08012345678",
-        password:        "dottle-nouveau-pavilion-tights-furze",
-        postal_code:     "1234567",
-        prefecture_name: "Tokyo",
-        address_city:    "tyuuou",
-        address_street:  "nihonbashi"
+        name:                   "Sumner",
+        email:                  "tester@example.com",
+        phone:                  "08012345678",
+        password:               "dottle-nouveau",
+        password_confirmation:  "dottle-nouveau",
+        postal_code:            "1234567",
+        prefecture_name:        "Tokyo",
+        address_city:           "tyuuou",
+        address_street:         "nihonbashi"
       )
       other_user = User.new(
-        name:            "Sumner2",
-        email:           "tester@example.com",
-        phone:           "08012345678",
-        password:        "dottle-nouveau-pavilion-tights-furze",
-        postal_code:     "1234567",
-        prefecture_name: "Tokyo",
-        address_city:    "tyuuou",
-        address_street:  "nihonbashi"
+        name:                   "Sumner2",
+        email:                  "tester@example.com",
+        phone:                  "08012345678",
+        password:               "dottle-nouveau",
+        password_confirmation:  "dottle-nouveau",
+        postal_code:            "1234567",
+        prefecture_name:        "Tokyo",
+        address_city:           "tyuuou",
+        address_street:         "nihonbashi"
       )
       other_user.valid?
       expect(other_user.errors[:email]).to include("は既に使用されています。")
+    end
+
+    # パスワードがなければ無効な状態であること
+    it "is invalid without a password" do
+      user.password = ''
+      user.valid?
+      expect(user.errors[:password]).to include("が入力されていません。")
     end
 
     # パスワードが5文字以下である場合は登録できないこと
@@ -129,7 +96,15 @@ RSpec.describe User, type: :model do
     # パスワードが6文字以上であれば登録できること
     it "is valid if the password is longer than 6 characters" do
       user.password = "dottle"
+      user.password_confirmation = "dottle"
       expect(user).to be_valid
+    end
+
+    # 郵便番号がなければ無効な状態であること
+    it "is invalid without a postal_code" do
+      user.postal_code = ''
+      user.valid?
+      expect(user.errors[:postal_code]).to include("を入力してください")
     end
 
     # 郵便番号が7文字以外であれば登録できないこと
@@ -137,6 +112,27 @@ RSpec.describe User, type: :model do
       user.postal_code = "123456"
       user.valid?
       expect(user.errors[:postal_code]).to include("は7文字で入力してください")
+    end
+
+    # 都道府県がなければ無効な状態であること
+    it "is invalid without a prefecture_name" do
+      user.prefecture_name = ''
+      user.valid?
+      expect(user.errors[:prefecture_name]).to include("を入力してください")
+    end
+
+    # 住所がなければ無効な状態であること
+    it "is invalid without a address_city" do
+      user.address_city = ''
+      user.valid?
+      expect(user.errors[:address_city]).to include("を入力してください")
+    end
+
+    # 建物名がなければ無効な状態であること
+    it "is invalid without a address_street" do
+      user.address_street  = ''
+      user.valid?
+      expect(user.errors[:address_street ]).to include("を入力してください")
     end
 
   #   # 市町村名が51文字以上であれば登録できないこと
