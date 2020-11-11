c45751f375c9ee0714fc3ca2b2db26362483b17f
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-11 18:22:53 +0900

    インデントの修正

--- spec/factories/facilities.rb
+++ spec/factories/facilities.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :facility do
-    sequence(:facility_name)     { |n| "sample#{n}" }
-    sequence(:email)             { |n| "TEST#{n}@example.com" }
-    password                     { "password" }
-    password_confirmation        { "password" }
+    sequence(:facility_name)  { |n| "sample#{n}" }
+    sequence(:email)          { |n| "TEST#{n}@example.com" }
+    password                  { "password" }
+    password_confirmation     { "password" }
   end
 end
