1f812933fe25e1f9e52cec996d1af0f83e3dd6bc
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-11 14:57:06 +0900

    informationモデルのテストを追加

--- spec/factories/information.rb
+++ spec/factories/information.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :information do
-    
+    association :facility
+    sequence(:news)   { |n| "news#{n}" }
+    sequence(:title)  { |n| "title#{n}" }
   end
 end
--- spec/models/information_spec.rb
+++ spec/models/information_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Information, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:information) { create(:information) }
+
+  describe 'validations' do
+    # タイトル、本文があれば有効な状態であること
+    it "is valid with a name, email, and password" do
+      expect(information).to be_valid
+    end
+
+    # タイトルがなければ無効な状態であること
+    it "is invalid without a title" do
+      information.title = ''
+      information.valid?
+      expect(information.errors[:title]).to include("を入力してください")
+    end
+
+    # 本文がなければ無効な状態であること
+    it "is invalid without a news" do
+      information.news = ''
+      information.valid?
+      expect(information.errors[:news]).to include("を入力してください")
+    end
+  end
 end
