b40f19a4c198f0c8e90e59c96bfa8f5ab486bc61
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-13 08:11:45 +0900

    # frozen_string_literal: trueの後に空行を入れること

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 # deviseサインアップ・編集用コントローラー
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :configure_sign_up_params, only: :create
