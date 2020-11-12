0b2e920f58729175754f50f35f1be5f32a6c124d
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-12 16:22:26 +0900

    重複したメソッドがある

--- app/controllers/facilities/registrations_controller.rb
+++ app/controllers/facilities/registrations_controller.rb
@@ -50,11 +50,6 @@ class Facilities::RegistrationsController < Devise::RegistrationsController
       devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute facility_name image icon remove_image remove_icon image_cache icon_cache])
     end
 
-    # The path used after sign up.
-    def after_sign_up_path_for(resource)
-      super(resource)
-    end
-
     # The path used after sign up for inactive accounts.
     def after_inactive_sign_up_path_for(resource)
       super(resource)
