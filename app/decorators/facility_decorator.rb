3f63b827f77e86178e61c5a791927d706af6a929
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-12 16:08:09 +0900

    配列内に余計なスペースをあけてはいけない

--- app/decorators/facility_decorator.rb
+++ app/decorators/facility_decorator.rb
@@ -6,13 +6,13 @@ module FacilityDecorator
       image_tag image.url, id: :img_prev, :size => '500x500'
     else
       month = Date.today.month
-      if month.in?([ 12, 1, 2 ])
+      if month.in?([12, 1, 2])
         image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/winter.jpg', id: :img_prev, :size => '500x500'
-      elsif month.in?([ 3, 4, 5 ])
+      elsif month.in?([3, 4, 5])
         image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/spring.jpg', id: :img_prev, :size => '500x500'
-      elsif month.in?([ 6, 7, 8 ])
+      elsif month.in?([6, 7, 8])
         image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/summer.jpg', id: :img_prev, :size => '500x500'
-      elsif month.in?([ 9, 10, 11 ])
+      elsif month.in?([9, 10, 11])
         image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/+autumn.jpg', id: :img_prev, :size => '500x500'
       end
     end
