6913b5bab5fc38ede991923f0baf89fb162e83f5
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-13 19:43:33 +0900

    不要な空行を削除

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_11_06_160505) do
-
   create_table "facilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
