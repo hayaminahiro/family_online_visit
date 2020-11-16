4cf9a0f3f338df03320bd7374af1579fc5520819
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-16 14:25:03 +0900

    relativeモデルの修正

--- app/models/relative.rb
+++ app/models/relative.rb
@@ -1,4 +1,11 @@
 class Relative < ApplicationRecord
   belongs_to :user
   belongs_to :resident
+
+  #search定義
+  def self.search(search, facility)
+    return facility.residents.where('name LIKE ?', "") unless search
+
+    facility.residents.where('name LIKE ?', "%#{search}%").order(:id)
+  end
 end
