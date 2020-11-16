a0b7716fe02c6b1b14c1e8d45a49392c7249253b
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-16 13:40:54 +0900

    informationモデルの修正

--- app/models/information.rb
+++ app/models/information.rb
@@ -8,4 +8,11 @@ class Information < ApplicationRecord
 
   # モデル | ImageUploaderクラスとimageカラムを紐づける
   mount_uploader :image, ImageUploader
+
+  #search定義
+  def self.search(search, facility)
+    return facility.informations.where(status: "others").order(id: "DESC") unless search
+
+    facility.informations.where(status: "others").where('title LIKE ?', "%#{search}%").order(id: "DESC")
+  end
 end
