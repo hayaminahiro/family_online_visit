a56f5516ab2d22cea8a9c697e744216e60d8ec80
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-16 14:15:54 +0900

    facility_userモデルの修正

--- app/models/facility_user.rb
+++ app/models/facility_user.rb
@@ -2,4 +2,11 @@ class FacilityUser < ApplicationRecord
   # optional: trueは、外部キーのnilを許可
   belongs_to :user, optional: true
   belongs_to :facility, optional: true
+
+  #search定義
+  def self.search(search, all, user)
+    return all.where('facility_name LIKE ?', "").where.not(admin: true) unless search
+
+    all.where('facility_name LIKE ?', "%#{search}%").where.not(admin: true, id: user.facilities).order(:id)
+  end
 end
