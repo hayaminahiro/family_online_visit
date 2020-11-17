ce6296621e62734418ee0ecf66eeb064c344465a
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-17 19:37:13 +0900

    searchが空だった際に施設が表示されないように修正

--- app/models/facility_user.rb
+++ app/models/facility_user.rb
@@ -5,8 +5,8 @@ class FacilityUser < ApplicationRecord
 
   #search定義
   def self.search(search, all, user)
-    return all.where('facility_name LIKE ?', "").where.not(admin: true) unless search
+    return all.where(['facility_name LIKE ?', "%#{search}%"]).where.not(admin: true, id: user.facilities).order(:id) if search.present?
 
-    all.where('facility_name LIKE ?', "%#{search}%").where.not(admin: true, id: user.facilities).order(:id)
+    all.where('facility_name LIKE ?', "").where.not(admin: true)
   end
 end
