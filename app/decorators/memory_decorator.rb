f7fca96d8d7b68ee91b03c8b637dd4bd0a0aed6d
Hayato Saito <omimisama@gmail.com>
2020-11-20 13:32:29 +0900

    モーダル編集/削除機能完了

--- app/decorators/memory_decorator.rb
+++ app/decorators/memory_decorator.rb
@@ -2,43 +2,6 @@
 
 module MemoryDecorator
 
-  # indexページ
-  def image00
-    image_tag image0.url, id: "img_prev_0_#{id}", class: "img-interval index-photo-size" if image0?
-  end
-
-  def image01
-    image_tag image1.url, id: "img_prev_1_#{id}", class: "img-interval index-photo-size" if image1?
-  end
-
-  def image02
-    image_tag image2.url, id: "img_prev_2_#{id}", class: "img-interval index-photo-size" if image2?
-  end
-
-  def image03
-    image_tag image3.url, id: "img_prev_3_#{id}", class: "img-interval index-photo-size" if image3?
-  end
-
-  def image04
-    image_tag image4.url, id: "img_prev_4_#{id}", class: "img-interval index-photo-size" if image4?
-  end
-
-  def image05
-    image_tag image5.url, id: "img_prev_5_#{id}", class: "img-interval index-photo-size" if image5?
-  end
-
-  def image06
-    image_tag image6.url, id: "img_prev_6_#{id}", class: "img-interval index-photo-size" if image6?
-  end
-
-  def image07
-    image_tag image7.url, id: "img_prev_7_#{id}", class: "img-interval index-photo-size" if image7?
-  end
-
-  def card_image
-    image_tag image0.url, class: "card-photo-size" if image0.present?
-  end
-
   # showページ
   def image00_s
     image_tag image0.url, class: "img-interval show-photo-size" if image0?
