cf41d4ce6123d261e142acc5b14b8ce0c6ad2e9f
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-20 18:10:20 +0900

    update_relativesをルーティングに追加した

--- config/routes.rb
+++ config/routes.rb
@@ -61,7 +61,9 @@ Rails.application.routes.draw do
     resources :request_residents, only: %i[new create index]
 
   # 入居者登録 ============================================================================
-    resources :relatives, except: %i[create edit]
+    resources :relatives, except: %i[create edit] do
+      patch :update_relatives, on: :collection
+    end
 
   # お知らせ ==============================================================================
     resources :informations do
