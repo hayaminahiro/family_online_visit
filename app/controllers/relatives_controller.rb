7293883526219c7129a1501b895daf959caf0d77
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-20 18:07:31 +0900

    relative_controllerの修正

--- app/controllers/relatives_controller.rb
+++ app/controllers/relatives_controller.rb
@@ -1,22 +1,36 @@
 class RelativesController < ApplicationController
-  before_action :set_request, only: %i[show destroy]
+  before_action :set_request, only: %i[show update index destroy]
+  before_action :set_residents, only: %i[new show update update_relatives]
 
   def new #家族から申請された内容を確認
-    @requests = RequestResident.where(facility_id:current_facility).where(req_approval: "申請中")
-    @residents = Relative.search(params[:search], current_facility)
   end
 
   def update #家族からの申請を承認・否認
     @user = User.find(params[:user_id].to_i)
     @request_resident = RequestResident.order(created_at: :desc).find_by(user_id: params[:user_id].to_i)
-    if (params[:user][:resident_ids] == ["", ""]) == true
-      flash[:alert] = "登録する入居者を選択してください。"
-    else
-      @user.update_attributes(residents_connection_params)
-      flash[:notice] = "入居者登録しました。"
+
+    @user.attributes = connection_params
+    if @user.save(context: :relative_update)
       @request_resident.承認済! #enumの値を「申請中→承認済」に更新
+      redirect_to facility_home_facility_url(current_facility), notice: "入居者登録しました。"
+    else
+      render :show
     end
-    redirect_to facility_home_facility_url(current_facility)
+  end
+
+  def update_relatives
+    residents_connection_params.each do |id, item|
+      @request_user = User.find(id)
+      @request_user.attributes = item
+      @request_resident = RequestResident.order(created_at: :desc).find_by(user_id: id)
+    end
+
+    if @request_user.save(context: :relative_update)
+      @request_resident.承認済! #enumの値を「申請中→承認済」に更新
+      redirect_to facility_home_facility_url(current_facility), notice: "入居者登録しました。"
+    else
+      render :new
+    end
   end
 
   def index #承認済み申請一覧
@@ -24,22 +38,30 @@ class RelativesController < ApplicationController
   end
 
   def show
-    @residents = Relative.search(params[:search], current_facility)
+    @user = User.find(@request.user_id)
   end
 
   def destroy
     @request.否認済! #enumの値を「申請中→否認」に更新
-    # @request.destroy
     redirect_to facility_home_facility_url(current_facility), notice: "[#{@request.req_name}][#{@request.req_phone}][#{@request.req_address}]の申請を否認しました。"
   end
 
     private
 
-      def residents_connection_params
+      def connection_params
         params.require(:user).permit(resident_ids: [])
+      end
+
+      def residents_connection_params
+        params.require(:facility).permit(request_residents:[resident_ids:[]])[:request_residents]
       end
 
       def set_request
         @request = RequestResident.find(params[:id])
+      end
+
+      def set_residents
+        @requests = RequestResident.where(facility_id:current_facility).where(req_approval: "申請中")
+        @residents = Relative.search(params[:search], current_facility)
       end
 end
