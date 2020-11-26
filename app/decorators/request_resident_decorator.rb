14331fdc73040f05dda24c092e5f8a1b381eb45d
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-26 12:03:03 +0900

    デコレータにviewの処理を移動した

--- app/decorators/request_resident_decorator.rb
+++ app/decorators/request_resident_decorator.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 module RequestResidentDecorator
+  def approval_status
+    if req_approval == "承認済"
+      "<span style= 'color: #e41414;'>#{req_approval}</span>".html_safe
+    else
+      "<span style= 'color: #a2b9ca;'>#{req_approval}</span>".html_safe
+    end
+  end
 end
