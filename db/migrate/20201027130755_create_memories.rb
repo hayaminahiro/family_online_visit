d13af677495489e47b54e1d93ee7f96d5f6ef73c
Hayato Saito <omimisama@gmail.com>
2020-11-27 01:44:56 +0900

    スキーマファイルの不具合を修正

--- db/migrate/20201027130755_create_memories.rb
+++ db/migrate/20201027130755_create_memories.rb
@@ -1,7 +1,6 @@
 class CreateMemories < ActiveRecord::Migration[5.2]
   def change
     create_table :memories do |t|
-      t.references :resident, foreign_key: true
       t.string :title
       t.text :message
       t.integer :add_image_id
