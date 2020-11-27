00201defe1b72507a08b311b40725fa26868ef41
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-11-27 18:13:53 +0900

    デコレータの記述を修正

--- app/decorators/request_resident_decorator.rb
+++ app/decorators/request_resident_decorator.rb
@@ -2,10 +2,6 @@
 
 module RequestResidentDecorator
   def approval_status
-    if req_approval == "承認済"
-      tag.span(req_approval, style: 'color: #e41414;')
-    else
-      tag.span(req_approval, style: 'color: #a2b9ca;')
-    end
+    req_approval == "承認済" ? tag.span(req_approval, style: 'color: #e41414;') : tag.span(req_approval, style: 'color: #a2b9ca;')
   end
 end
