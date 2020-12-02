5a94207ba68ab684207a75b311375784eb58781e
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-12-03 02:12:09 +0900

    モーダル表示、登録機能完了

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,21 +1,44 @@
 class RoomsController < ApplicationController
-  before_action :set_room, only: %i[craate edit update]
+  before_action :set_room, only: %i[create edit update]
+  before_action :set_user, only: %i[new create edit update]
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = @user.rooms.build(room_params)
+    if @room.save
+      redirect_to users_url, notice: "Room Nameを登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
+    @room = @user.rooms.find_by(id: params[:id])
   end
 
   def update
+    @room = @user.rooms.find_by(id: params[:id])
+    if @room.update_attributes(room_params)
+      redirect_to users_url, notice: "Room Nameを更新しました。"
+    else
+      render :edit
+    end
   end
 
   private
 
+    def room_params
+      params.require(:room).permit(:room_name, :facility_id)
+    end
+
     def set_room
       @room = Room.find(params[:id])
+    end
+
+    def set_user
+      @user = User.find(params[:user_id])
     end
 end
