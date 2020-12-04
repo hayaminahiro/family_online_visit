faa676b05a6c19390e3d264ada44c3b6d0f8ac60
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-04 14:23:09 +0900

    過去の申請一覧画面の名前表示部分の処理をデコレータに移した

--- app/decorators/request_resident_decorator.rb
+++ app/decorators/request_resident_decorator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module RequestResidentDecorator
-  def approval_status
+  def status
     req_approval == "承認済" ? tag.span(req_approval, style: 'color: #e41414;') : tag.span(req_approval, style: 'color: #a2b9ca;')
   end
 
@@ -13,6 +13,14 @@ module RequestResidentDecorator
       tag.p((link_to "利用者登録は #{request.req_approval} です", new_request_resident_path(facility_id: facility), class: "btn-shine"))
     else
       tag.p((link_to "ご利用者様の登録申請", new_request_resident_path(facility_id: facility), class: "btn-shine"))
+    end
+  end
+
+  def user_name(user)
+    if id == RequestResident.order(id: :desc).where(facility_id: current_facility).find_by(user_id: user_id).id
+      tag.p((link_to user.name, edit_relative_path(id), class: "is-small is-size-6", style: "width: 45%;"))
+    else
+      user.name
     end
   end
 end
