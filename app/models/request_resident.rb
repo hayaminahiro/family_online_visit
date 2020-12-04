d55eb8287466b95bd4f863d18ef178731f5f118e
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-04 16:22:41 +0900

    rubocopからの指摘を修正

--- app/models/request_resident.rb
+++ app/models/request_resident.rb
@@ -7,8 +7,8 @@ class RequestResident < ApplicationRecord
 
   enum req_approval: { "申請中": 0, "承認済": 1, "否認済": 2 }
 
-  scope :applied, -> (facility){ where(facility_id: facility).where.not(req_approval: "申請中") }
-  scope :active, -> (facility){ where(facility_id: facility).where(req_approval: "申請中") }
+  scope :applied, ->(facility) { where(facility_id: facility).where.not(req_approval: "申請中") }
+  scope :active, ->(facility) { where(facility_id: facility).where(req_approval: "申請中") }
 
   def self.changer(facility, user)
     order(created_at: :desc).where(facility_id: facility).find_by(user_id: user)
