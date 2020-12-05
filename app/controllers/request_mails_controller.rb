344790b94d1fc4a1957e21c2f7f12b809e3696e9
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-05 12:14:12 +0900

    確認メールを送る際のメッセージを修正しました

--- app/controllers/request_mails_controller.rb
+++ app/controllers/request_mails_controller.rb
@@ -11,12 +11,13 @@ class RequestMailsController < ApplicationController
 
   def send_mail
     request = RequestResident.find(params[:request_mail][:facility_id].to_i)
+    user = User.find(params[:request_mail][:user_id].to_i)
     # フォームで入力された値の取得
     title = params[:request_mail][:title]
     message = params[:request_mail][:message]
     RequestMailer.send_confirm_to_user(current_facility, request, title, message).deliver
 
-    redirect_to facility_home_facility_url(current_facility), notice: "[#{request.req_name}][#{request.req_phone}][#{request.req_address}]の申請を否認しました。"
+    redirect_to facility_home_facility_url(current_facility), notice: "#{user.name}様へ確認メールを送信しました。"
   end
 
   # private
