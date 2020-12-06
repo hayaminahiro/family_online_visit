96331c2c90f37e0223f43b7372dd283d533399b6
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-06 22:47:51 +0900

    検索フォームの機能を拡張した

--- app/models/request_resident.rb
+++ app/models/request_resident.rb
@@ -9,6 +9,12 @@ class RequestResident < ApplicationRecord
 
   scope :applied, ->(facility) { includes(:user).where(facility_id: facility).where.not(req_approval: "申請中")  }
   scope :active, ->(facility) { includes(user: :residents).where(facility_id: facility).where(req_approval: "申請中") }
+  scope :search_columns, ->(search) do
+    where('users.name LIKE ?', "%#{search}%").
+      or(where('req_name LIKE ?', "%#{search}%")).
+        or(where('req_phone LIKE ?', "%#{search}%")).
+          or(where('req_address LIKE ?', "%#{search}%"))
+  end
 
   def self.changer(facility, user)
     order(created_at: :desc).where(facility_id: facility).find_by(user_id: user)
@@ -16,7 +22,7 @@ class RequestResident < ApplicationRecord
 
   # search定義
   def self.search(search, facility)
-    return joins(:user).includes(:user).where(facility_id: facility).where('users.name LIKE ?', "%#{search}%").order(updated_at: :desc) if search.present?
+    return joins(:user).includes(:user).where(facility_id: facility).search_columns(search).order(updated_at: :desc) if search.present?
 
     includes(:user).where(facility_id: facility).order(updated_at: :desc)
   end
