941f93bb02c323a274ee1b263a642ce900038166
Hayato Saito <omimisama@gmail.com>
2020-12-06 22:36:12 +0900

    carriawave.rbインデント調整

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,17 +4,17 @@ require 'carrierwave/storage/fog'
 
 if Rails.env.production?
   CarrierWave.configure do |config|
-      config.storage :fog
-      config.fog_provider = 'fog/aws'
-      config.fog_directory = ENV['AWS_S3_BUCKET']
-      config.fog_credentials = {
-        provider: 'AWS',
-        aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-        aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-        region: 'ap-northeast-1',
-        path_style: true,
-      }
-      config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+    config.storage :fog
+    config.fog_provider = 'fog/aws'
+    config.fog_directory = ENV['AWS_S3_BUCKET']
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'ap-northeast-1',
+      path_style: true,
+    }
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   end
 
   # 日本語ファイル名の設定
