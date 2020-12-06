4d399a2c187276c36fae3e608f05b9da7490f40c
yuya0311 <toyo.yuya0311@gmail.com>
2020-12-06 21:24:09 +0900

    conflict解消2

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_12_01_064253) do
-=======
-ActiveRecord::Schema.define(version: 2020_11_06_160505) do
->>>>>>> 9760a8115db38298bee42b36ab5406713edb4a7b
 
   create_table "facilities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "facility_name"
