721a8064f8f2c5e9d99ca13eccb58982a5d9669d
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-06 17:30:53 +0900

    デコレータの使われていない定義を削除

--- app/decorators/request_resident_decorator.rb
+++ app/decorators/request_resident_decorator.rb
@@ -2,7 +2,14 @@
 
 module RequestResidentDecorator
   def status
-    req_approval == "承認済" ? tag.span(req_approval, style: 'color: #e41414;') : tag.span(req_approval, style: 'color: #a2b9ca;')
+    case req_approval
+    when "承認済"
+      tag.span(req_approval, style: 'color: #e41414;')
+    when "否認済"
+      tag.span(req_approval, style: 'color: #0c94f9;')
+    else
+      tag.span(req_approval, style: 'color: #a2b9ca;')
+    end
   end
 
   def approval_link(request, facility)
@@ -13,14 +20,6 @@ module RequestResidentDecorator
       tag.p((link_to "利用者登録は #{request.req_approval} です", new_request_resident_path(facility_id: facility), class: "btn-shine"))
     else
       tag.p((link_to "ご利用者様の登録申請", new_request_resident_path(facility_id: facility), class: "btn-shine"))
-    end
-  end
-
-  def user_name
-    if id == RequestResident.order(id: :desc).where(facility_id: current_facility).find_by(user_id: user_id).id
-      tag.p((link_to user.name, edit_relative_path(id), class: "is-small is-size-6", style: "width: 45%;"))
-    else
-      user.name
     end
   end
 end
