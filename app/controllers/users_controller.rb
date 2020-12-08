ea1fe64b8c8889aedac308dc95d50e354181afb0
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-12-09 02:02:37 +0900

    Room Nameの設定なしではビデオチャットできないように変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[edit update destroy video_room]
-  # before_action :set_facility_id, only: %i[index video_room]
+  before_action :set_user, only: %i[edit update destroy video_room room_name_required]
+  before_action :set_facility_id, only: %i[video_room room_name_required]
   # ログインしてなければ閲覧不可
   before_action :authenticate_user!, except: %i[index video_room edit update destroy]
   before_action :authenticate_facility!, only: %i[index edit update destroy]
+  before_action :room_name_required, only: :video_room
 
   def index
     @users = User.search(params[:search], current_facility).paginate(page: params[:page], per_page: 30)
@@ -34,7 +35,6 @@ class UsersController < ApplicationController
   end
 
   def video_room
-    @facility = Facility.find_by(id: params[:facility_id])
     @room = Room.find_by(user_id: @user.id, facility_id: @facility.id)
   end
 
@@ -52,9 +52,21 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    # def set_facility_id
-    #   @facility = Facility.find(params[:facility_id])
-    # end
+    def set_facility_id
+      @facility = Facility.find(params[:facility_id])
+    end
+
+    # ビデオチャットでのRoom Nameの設定のお知らせ
+    def room_name_required
+      @room = Room.find_by(user_id: @user.id, facility_id: @facility.id)
+      if @room.nil?
+        if current_facility
+          redirect_to users_url, alert: "#{@user.name}の Room Name を設定してください。"
+        elsif current_user
+          redirect_to facility_home_url(@facility), alert: "ビデオチャットが準備中のため入室できません。"
+        end
+      end
+    end
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
