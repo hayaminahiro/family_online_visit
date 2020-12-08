bff0e4d0eac0354fa56bcaab14f940eaa02eb331
yuya0311 <toyo.yuya0311@gmail.com>
2020-12-08 16:40:40 +0900

    ラベル横に必須追加

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -1,8 +1,8 @@
 class InquiriesController < ApplicationController
+  before_action :set_facility_id, only: %i[inquiry create index]
   before_action :authenticate_user!, only: %i[inquiry create]
 
   def inquiry
-    @facility = Facility.find(params[:facility_id])
     if params[:name].present?
       @inquiry = Inquiry.new(inquiry_params)
     else
@@ -19,13 +19,12 @@ class InquiriesController < ApplicationController
   end
 
   def create
-    @facility = Facility.find(params[:facility_id])
     @inquiry = Inquiry.new(inquiry_params)
     if @inquiry.save
       InquiryMailer.send_mail(@inquiry).deliver_now
       redirect_to inquiry_facility_inquiries_path(@facility), notice: "お問い合わせを完了しました。"
     else
-      render "inquiry"
+      render :inquiry
     end
   end
 
@@ -35,17 +34,20 @@ class InquiriesController < ApplicationController
       InquiryMailer.send_mail(@inquiry).deliver_now
       redirect_to inquiry_system_inquiries_path, notice: "お問い合わせを完了しました。"
     else
-      render "inquiry_system"
+      render :inquiry_system
     end
   end
 
   def index
-    @facility = Facility.find(params[:facility_id])
     @inquiry = Inquiry.where(facility_id: @facility.id)
   end
 
   private
     def inquiry_params
       params.require(:inquiry).permit(:name, :email, :message, :facility_id)
+    end
+
+    def set_facility_id
+      @facility = Facility.find(params[:facility_id])
     end
 end
