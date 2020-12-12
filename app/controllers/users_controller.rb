fb26ace0cca053759cc4fb1101adaa9b8b32a1a2
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-12-12 11:42:25 +0900

    ビデオチャットページへの遷移修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user,               only: %i[edit update destroy video_room set_room]
   before_action :set_facility_id,        only: %i[video_room set_room]
-  before_action :set_room,               only: %i[room_required_facility room_required_user]
+  before_action :set_room,               only: :room_required
   # ログインしてなければ閲覧不可
   before_action :authenticate_user!,     except: %i[index video_room edit update destroy]
   before_action :authenticate_facility!, only: %i[index edit update destroy]
-  before_action :room_required_facility, only: :video_room
-  before_action :room_required_user,     only: :video_room
+  before_action :room_required, only: :video_room
 
   def index
     @users = User.search(params[:search], current_facility).paginate(page: params[:page], per_page: 30)
@@ -63,16 +62,14 @@ class UsersController < ApplicationController
     end
 
     # ビデオチャットでのRoom Nameの設定のお知らせ
-    def room_required_facility
-      return unless @room.nil? && current_facility
+    def room_required
+      return true unless @room.nil?
 
-      redirect_to users_url, alert: "#{@user.name}の Room Name を設定してください。"
-    end
-
-    def room_required_user
-      return unless @room.nil? && current_user
-
-      redirect_to facility_home_path(@facility), alert: "ビデオチャットの準備ができていません、今しばらくお待ちください。"
+      if current_facility 
+        redirect_to users_url, alert: "#{@user.name}の Room Name を設定してください。"
+      elsif current_user
+        redirect_to facility_home_path(@facility), alert: "ビデオチャットの準備ができていません、今しばらくお待ちください。"
+      end
     end
 
     def user_params
