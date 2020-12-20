cd1b900359004fa047158113a5ea1f7f750fcae1
yuya0311 <toyo.yuya0311@gmail.com>
2020-12-20 17:14:54 +0900

    pull後

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,6 +1,6 @@
 class Inquiry < ApplicationRecord
   # belongs_to :facility
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
 
   validates :name,      presence: true
   validates :email,     presence: true, length: { maximum: 100 }, format: { with: VALID_EMAIL_REGEX }
