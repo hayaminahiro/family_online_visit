d416cd4b91825a15c0fc011ceddef9fdaa404427
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-23 13:48:44 +0900

    tag_imagesモデルの作成

---
+++ app/controllers/tag_images_controller.rb
@@ -0,0 +1,24 @@
+class TagImagesController < ApplicationController
+  def new
+    @tag_image = TagImage.new
+  end
+
+  def create
+    @tag_image = current_user.tag_images.build(tag_image_params)
+    respond_to do |format|
+      if @tag_image.save
+        format.html
+        format.js
+      # else
+      #   format.js {render :new}
+      end
+    end
+  end
+
+  private
+
+    def tag_image_params
+      params.require(:user).permit(:image, :tag_id)
+      # params.permit(:image, :tag_id)
+    end
+end
---
+++ spec/requests/tag_images_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "TagImages", type: :request do
+
+end
