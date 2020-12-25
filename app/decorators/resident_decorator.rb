b4509cea1d55df9de4514562e296fe796b32336c
takahiro-nagano <taka_5393@yahoo.co.jp>
2020-12-25 11:56:14 +0900

    デコレータに移動した処理

--- app/decorators/resident_decorator.rb
+++ app/decorators/resident_decorator.rb
@@ -4,4 +4,19 @@ module ResidentDecorator
   def modal_title
     id.present? ? "入居者情報編集" : "入居者新規登録"
   end
+
+  def image_columns
+    image_columns = []
+    memories.each do |m|
+      image_columns << m.image0 if m.image0?
+      image_columns << m.image1 if m.image1?
+      image_columns << m.image2 if m.image2?
+      image_columns << m.image3 if m.image3?
+      image_columns << m.image4 if m.image4?
+      image_columns << m.image5 if m.image5?
+      image_columns << m.image6 if m.image6?
+      image_columns << m.image7 if m.image7?
+    end
+    image_columns
+  end
 end
