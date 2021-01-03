3e442b4187d0e00bacbb8a1789d97ad903430e17
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-03 11:20:16 +0900

    タブに追加した画像を削除できるようにした

--- app/controllers/tag_images_controller.rb
+++ app/controllers/tag_images_controller.rb
@@ -5,10 +5,25 @@ class TagImagesController < ApplicationController
 
   def create
     @tag_image = current_user.tag_images.build(tag_image_params)
+
     respond_to do |format|
-      if @tag_image.save
+      if !@tag_image.tag.tag_images.map(&:image).include?(@tag_image.image)
+        @tag_image.save
         format.html
         format.js { flash.now[:notice] = "#{Tag.find(@tag_image.tag_id).title}フォルダに画像を追加しました" }
+      else
+        format.js { flash.now[:alert] = "#{Tag.find(@tag_image.tag_id).title}フォルダにはすでに同じ画像が登録されています" }
+      end
+    end
+  end
+
+  def destroy
+    @tag_image = TagImage.find(params[:id])
+
+    respond_to do |format|
+      if @tag_image.destroy
+        format.html
+        format.js { flash.now[:alert] = "#{Tag.find(@tag_image.tag_id).title}フォルダから画像を削除しました" }
         # else
         # format.js { render :new }
       end
