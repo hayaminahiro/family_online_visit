434b82006d3b62cacd411bee5e698d5626f8c1aa
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-01-09 09:31:33 +0900

    施設背景画像モバイル対応完了

--- app/decorators/facility_decorator.rb
+++ app/decorators/facility_decorator.rb
@@ -39,17 +39,17 @@ module FacilityDecorator
   # 施設編集（モバイル）背景画像
   def facility_edit_background_image
     if image?
-      image_tag image.url, id: :img_prev, class: "home-image", onClick: "$('.image_uploading').click()"
+      image_tag image.url, id: :mobile_prev, class: "home-image", onClick: "$('.image_uploading_mobile').click()"
     else
       month = Time.zone.today.month
       if month.in?([12, 1, 2])
-        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/winter.jpg', id: :img_prev, class: "home-image", onClick: "$('.image_uploading').click()"
+        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/winter.jpg', id: :mobile_prev, class: "home-image", onClick: "$('.image_uploading_mobile').click()"
       elsif month.in?([3, 4, 5])
-        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/spring.jpg', id: :img_prev, class: "home-image", onClick: "$('.image_uploading').click()"
+        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/spring.jpg', id: :mobile_prev, class: "home-image", onClick: "$('.image_uploading_mobile').click()"
       elsif month.in?([6, 7, 8])
-        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/summer.jpg', id: :img_prev, class: "home-image", onClick: "$('.image_uploading').click()"
+        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/summer.jpg', id: :mobile_prev, class: "home-image", onClick: "$('.image_uploading_mobile').click()"
       elsif month.in?([9, 10, 11])
-        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/+autumn.jpg', id: :img_prev, class: "home-image", onClick: "$('.image_uploading').click()"
+        image_tag 'https://img-photo.s3-ap-northeast-1.amazonaws.com/uploads/content_image/+autumn.jpg', id: :mobile_prev, class: "home-image", onClick: "$('.image_uploading_mobile').click()"
       end
     end
   end
