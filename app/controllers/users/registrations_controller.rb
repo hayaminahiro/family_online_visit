e5f92db34d22a6c41eed7987a73bac3a582979a4
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-01-12 15:29:24 +0900

    ヘッダーアイコンのカラム変更、電話番号、住所関係の編集追加

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -58,7 +58,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
     # If you have extra params to permit, append them to the sanitizer.
     def configure_account_update_params
       # devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-      devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute image remove_image image_cache]) # facility_idsを追加
+      devise_parameter_sanitizer.permit(:account_update, keys: %i[attribute name email phone postal_code prefecture_name address_city address_street image remove_image image_cache icon remove_icon icon_cache]) # facility_idsを追加
     end
 
     def update_resource(resource, params)
