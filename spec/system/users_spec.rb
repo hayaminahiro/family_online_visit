15be26fe2cc254f6bed0dd182acb8eeb0fcf30d2
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-13 18:27:17 +0900

    user_specのテストを修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -21,23 +21,25 @@ RSpec.describe 'Sessions', type: :system do
       end
       context '新規登録画面に遷移' do
         it '新規登録に成功する' do
+          # step1
           fill_in 'あなたのお名前', with: "test1_user"
           fill_in 'メールアドレス', with: "test1@email.com"
           fill_in 'パスワード', with: "password"
           fill_in 'パスワード再確認', with: "password"
-
           # 次へボタンをクリック
           find('.button.is-link.is-medium.is-fullwidth.new-btn.has-text-weight-bold').click
 
+          # step2
           fill_in 'zip', with: '1000000'
           sleep 5
           fill_in '都道府県', with: '東京都'
           fill_in '市区町村', with: '千代田区'
           fill_in '町域', with: '123456'
           fill_in '電話番号', with: '08012345678'
+          # 次へボタンをクリック
           find('.button.is-link.is-medium.is-fullwidth.new-btn.has-text-weight-bold').click
 
-          # 新規登録ボタンをクリック
+          # 確認画面/新規登録ボタンをクリック
           find('.button.is-link.is-medium.success-btn.has-text-weight-bold').click
 
           expect(page).to have_content '新規ユーザーを登録しました'
