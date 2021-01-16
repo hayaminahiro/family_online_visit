22f610a84c2bb3bc70d80d55af51913426101f02
Hayato Saito <omimisama@gmail.com>
2021-01-16 19:23:54 +0900

    カレンダー詳細設定頁作成

--- app/controllers/calendar_settings_controller.rb
+++ app/controllers/calendar_settings_controller.rb
@@ -1,4 +1,12 @@
 class CalendarSettingsController < ApplicationController
   def index
+
+    
+    @facility = Facility.find(current_facility.id)
+    @reservations = Reservation.all
+    @reservations_facility_max = @reservations.facility(current_facility)
+    @reservations_facility_day_max = @reservations_facility_max.reservation_user(@user.name) if @user.present?
+    @reservations_user_max = @reservations.facility(@facility)
+    @reservations_user_day_max = @reservations_user_max.reservation_user(current_user.name) if current_user.present?
   end
 end
