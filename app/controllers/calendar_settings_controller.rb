0f36d050bea907f716744f4c7c7d2e754e41c1d5
Hayato Saito <omimisama@gmail.com>
2021-01-16 22:51:11 +0900

    モバイル表示の予約件数表示調整

--- app/controllers/calendar_settings_controller.rb
+++ app/controllers/calendar_settings_controller.rb
@@ -1,9 +1,7 @@
 class CalendarSettingsController < ApplicationController
   def index
-
-    
     @facility = Facility.find(current_facility.id)
-    @reservations = Reservation.all
+    @reservations = Reservation.all.sorted
     @reservations_facility_max = @reservations.facility(current_facility)
     @reservations_facility_day_max = @reservations_facility_max.reservation_user(@user.name) if @user.present?
     @reservations_user_max = @reservations.facility(@facility)
