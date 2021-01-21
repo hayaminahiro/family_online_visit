aefb68a4211fe4dadc4ccfbb8986c552c560a18b
Hayato Saito <omimisama@gmail.com>
2021-01-21 12:11:13 +0900

    設定時間のチェックボックス修正

--- app/models/calendar_setting.rb
+++ app/models/calendar_setting.rb
@@ -2,6 +2,7 @@ class CalendarSetting < ApplicationRecord
   belongs_to :facility
 
   DayOfTheWeek =["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"].freeze
+  ReservationTimes =["10:00 ~ 10:30", "10:30 ~ 11:00", "11:00 ~ 11:30", "11:30 ~ 12:00", "14:00 ~ 14:30", "14:30 ~ 15:00", "15:00 ~ 15:30", "15:30 ~ 16:00", "16:00 ~ 16:30", "16:30 ~ 17:00"].freeze
 
 
   scope :facility, ->(facility) { where(facility_id: facility) }
