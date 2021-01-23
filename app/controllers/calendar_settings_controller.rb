b5329c01ff3b51ee37f5f98da1814a2fa5cab9e8
Hayato Saito <omimisama@gmail.com>
2021-01-23 13:13:20 +0900

    定休日と非表示時間をカレンダーに反映

--- app/controllers/calendar_settings_controller.rb
+++ app/controllers/calendar_settings_controller.rb
@@ -9,6 +9,19 @@ class CalendarSettingsController < ApplicationController
 
     @calendar_settings = CalendarSetting.all
     @set_id = @calendar_settings.find_by(facility_id: current_facility.id).id if @calendar_settings.find_by(facility_id: current_facility.id).present?
+
+    
+    if current_facility.present?
+      if @calendar_settings.find_by(facility_id: current_facility.id).present?
+        @sunday = 0 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("日曜日")
+        @monday = 1 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("月曜日")
+        @tuesday = 2 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("火曜日")
+        @wednesday = 3 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("水曜日")
+        @thursday = 4 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("木曜日")
+        @friday = 5 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("金曜日")
+        @saturday = 6 if @calendar_settings.find_by(facility_id: current_facility.id).regular_holiday.include?("土曜日")
+      end
+    end
   end
 
   def new
