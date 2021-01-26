33c24127601fe08a531f9027ded23b340edfd7ba
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-27 07:59:17 +0900

    production.rbにメール機能に関わる記述を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    user_name: ENV['USER_NAME'],
+    password: ENV['USER_PASSWORD'],
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   config.action_mailer.default_url_options = { host: "family-online-visit.herokuapp.com" }
 end
