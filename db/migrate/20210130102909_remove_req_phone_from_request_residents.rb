e368ac8632bcf456c24d77b104b825bf9ee9dd07
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-31 18:19:20 +0900

    rubocopの修正

--- db/migrate/20210130102909_remove_req_phone_from_request_residents.rb
+++ db/migrate/20210130102909_remove_req_phone_from_request_residents.rb
@@ -1,8 +1,6 @@
 class RemoveReqPhoneFromRequestResidents < ActiveRecord::Migration[5.2]
   def change
-    change_table :request_residents, bulk: true do |t|
-      t.remove :req_phone
-      t.remove :req_address
-    end
+    remove_column :request_residents, :req_phone, :string
+    remove_column :request_residents, :req_address, :string
   end
 end
