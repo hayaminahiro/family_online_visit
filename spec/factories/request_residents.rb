5f4bbd85149954cc812286d21738a2434356c12a
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-31 17:00:39 +0900

    テストファイルの不要なコードを削除

--- spec/factories/request_residents.rb
+++ spec/factories/request_residents.rb
@@ -2,7 +2,5 @@ FactoryBot.define do
   factory :request_resident do
     association :user
     sequence(:req_name)  { |n| "sample#{n}" }
-    req_phone            { "08012345678" }
-    req_address          { "Tokyo" }
   end
 end
