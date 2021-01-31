26ec0c0247fcc28c7f703cc829238bae0827559d
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-01-31 17:52:47 +0900

    rubocopの指摘を修正

--- db/migrate/20210130102909_remove_req_phone_from_request_residents.rb
+++ db/migrate/20210130102909_remove_req_phone_from_request_residents.rb
@@ -1,6 +1,8 @@
 class RemoveReqPhoneFromRequestResidents < ActiveRecord::Migration[5.2]
   def change
-    remove_column :request_residents, :req_phone, :string
-    remove_column :request_residents, :req_address, :string
+    change_table :request_residents, bulk: true do |t|
+      t.remove :req_phone
+      t.remove :req_address
+    end
   end
 end
--- spec/factories/request_residents.rb
+++ spec/factories/request_residents.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :request_resident do
     association :user
-    sequence(:req_name)  { |n| "sample#{n}" }
+    sequence(:req_name) { |n| "sample#{n}" }
   end
 end
