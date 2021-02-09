e319962d00dc832b4c434d2370cd9f50767ea1c5
takanori_kaku <kaku.takanori.1026@gmail.com>
2021-02-09 22:11:43 +0900

    ログイン後の遷移先変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -28,7 +28,7 @@ class Users::SessionsController < Devise::SessionsController
     devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   end
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(*)
     user = current_user.facilities
     user.present? ? user_path(current_user) : new_facility_user_path
   end
