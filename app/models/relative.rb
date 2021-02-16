b09143e5d2a303b58e3ccdeb71115eed4a7e9f2c
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-02-16 11:00:17 +0900

    モデルに更新処理で使用のメソッド追加

--- app/models/relative.rb
+++ app/models/relative.rb
@@ -8,4 +8,15 @@ class Relative < ApplicationRecord
 
     facility.residents
   end
+
+  def self.reject_zero(resident_ids)
+    resident_ids.map(&:to_i).reject(&:zero?).sort
+  end
+
+  def self.set_update_ids(update_ids)
+    update_ids[:set_ids] = [] if update_ids[:set_ids].nil?
+    new_resident_ids = update_ids[:resident_ids].concat(update_ids[:set_ids])
+    update_ids.delete(:set_ids)
+    self.reject_zero(new_resident_ids)
+  end
 end
