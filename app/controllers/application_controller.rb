f04aae83a5ed45931bae5dbb1fb31e4f6417d83c
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-02-19 16:28:03 +0900

    エラー画面を表示させないように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,29 +3,29 @@ class ApplicationController < ActionController::Base
   before_action :configre_permitted_parameters, if: :devise_controller?
 
   # 例外処理
-  rescue_from Exception, with: :render_500
-  rescue_from ActiveRecord::RecordNotFound, with: :render_404
-  rescue_from ActionController::RoutingError, with: :render_404
+  # rescue_from Exception, with: :render_500
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  # rescue_from ActionController::RoutingError, with: :render_404
 
-  def render_500(exception = nil)
-    logger.info "Rendering 500 with exception: #{exception.message}" if exception
+  # def render_500(exception = nil)
+  #   logger.info "Rendering 500 with exception: #{exception.message}" if exception
 
-    if request.xhr?
-      render json: { error: '500 error' }, status: 500
-    else
-      render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
-    end
-  end
+  #   if request.xhr?
+  #     render json: { error: '500 error' }, status: 500
+  #   else
+  #     render template: 'errors/error_500', status: 500, layout: 'application', content_type: 'text/html'
+  #   end
+  # end
 
-  def render_404(exception = nil)
-    logger.info "Rendering 404 with exception: #{exception.message}" if exception
+  # def render_404(exception = nil)
+  #   logger.info "Rendering 404 with exception: #{exception.message}" if exception
 
-    if request.xhr?
-      render json: { error: '404 error' }, status: 404
-    else
-      render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
-    end
-  end
+  #   if request.xhr?
+  #     render json: { error: '404 error' }, status: 404
+  #   else
+  #     render template: 'errors/error_404', status: 404, layout: 'application', content_type: 'text/html'
+  #   end
+  # end
 
   # deviseでユーザー新規作成の際に名前のカラムを保存するためのもの（標準装備はemail,passwordのみ）
   def configre_permitted_parameters
