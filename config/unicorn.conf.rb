b3647f3b9de080991a15158e376f5c2972930f8f
Hayato Saito <omimisama@gmail.com>
2021-02-28 00:35:38 +0900

    Rubocop修正

--- config/unicorn.conf.rb
+++ config/unicorn.conf.rb
@@ -1,6 +1,7 @@
 $worker  = 2
 $timeout = 30
-$app_dir = "/app_name" #自分のアプリケーションまでのpath
+# 自分のアプリケーションまでのpath
+$app_dir = "/app_name"
 $listen  = File.expand_path 'tmp/sockets/.unicorn.sock', $app_dir
 $pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
 $std_log = File.expand_path 'log/unicorn.log', $app_dir
