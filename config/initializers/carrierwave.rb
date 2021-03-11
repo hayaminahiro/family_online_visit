c72941ba40f1b54765b1863506f8e0da4e259cf0
Hayato Saito <omimisama@gmail.com>
2021-03-11 11:15:44 +0900

    Rubocop修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,7 @@ require 'carrierwave/storage/fog'
 
 # AWS
 CarrierWave.configure do |config|
-  if Rails.env.production? # 本番環境:AWS使用
+  if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory = Rails.application.credentials.aws[:bucket]
@@ -17,8 +17,8 @@ CarrierWave.configure do |config|
     }
     config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
   else
-    config.storage :file # 開発環境:public/uploades下に保存
-    config.enable_processing = false if Rails.env.test? #test:処理をスキップ
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
 
