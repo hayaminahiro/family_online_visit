9e38e5bef1a195e49ebc291051ae1c506d8a8100
takahiro-nagano <taka_5393@yahoo.co.jp>
2021-03-21 17:26:35 +0900

    動画投稿フォームを追加

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,11 +1,11 @@
 class InformationsController < ApplicationController
   # ログインしてなければ閲覧不可
-  before_action :authenticate_user!, only: %i[top_notice show]
+  before_action :authenticate_user!, only: %i[show]
   before_action :authenticate_facility!, only: %i[index create new update destroy]
   before_action :set_information, only: %i[show edit update destroy]
 
   def index
-    @info_top = Information.find_by(status: "head")
+    @admin_informations = Information.where(status: "admin").order(id: "DESC")
     @informations = Information.search(params[:search], current_facility).paginate(page: params[:page], per_page: 9)
   end
 
@@ -20,8 +20,9 @@ class InformationsController < ApplicationController
   end
 
   def create
-    @information = Information.new(information_params)
-    @information.facility_id = current_facility.id
+    @information = current_facility.informations.build(information_params)
+    @information.url = information_params[:url].last(11)
+    @information.admin! if current_facility.admin == true
     if @information.save
       redirect_to informations_url, notice: "タイトル【#{@information.title}】/お知らせを新規作成できました。"
     else
@@ -35,7 +36,7 @@ class InformationsController < ApplicationController
     @information.facility_id = current_facility.id
     if @information.update(information_params)
       flash[:notice] = "タイトル【#{@information.title}】/お知らせを更新できました。"
-      @information.status == "head" ? redirect_to(facility_home_facility_url) : redirect_to(informations_url)
+      @information.status == "admin" ? redirect_to(facility_home_facility_url) : redirect_to(informations_url)
     else
       render :edit
     end
@@ -46,16 +47,10 @@ class InformationsController < ApplicationController
     redirect_to informations_url, alert: "お知らせを削除しました"
   end
 
-  # 家族向けお知らせ表示ページ
-  def top_notice
-    @info_top = Information.find_by(status: "head")
-    @informations = Information.where(facility_id: params[:facility_id].to_i).where(status: "others").order(id: "DESC")
-  end
-
   private
 
     def information_params
-      params.require(:information).permit(:news, :title, :image, :remove_image, :image_cache)
+      params.require(:information).permit(:news, :title, :image, :remove_image, :image_cache, :url)
     end
 
     def set_information
